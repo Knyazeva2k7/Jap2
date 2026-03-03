593f09fe29f41aa2ceeb1f99202866c96196b115
Diana <knyazevaofficial@gmail.com>
2026-03-03 21:24:45 +0700

    Add files via upload

--- Header.h
+++ Header.h
@@ -1,21 +1,66 @@
 #pragma once
 #include <string>
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Shoes {
+class Bell {
 public:
-	string name;
-	int size;
-	double weight;
-	string gender;
-	
-
-	Shoes(string name, double weight, int size, string gender);
-	void print();
+	void sound();
 };
 
-void sortBubbleSize(vector<Shoes>& shoes);
+class OddEvenSeparator {
+private:
+	vector<int> evenInt;
+	vector<int> oddInt;
+public:
+	void even();
+	void odd();
+	void add_number(int n);
+};
 
-void sortBubbleWeight(vector<Shoes>& shoes);
+void menuTable();
+
+class Table {
+private:
+	vector <vector<int>> tables;//двумерный вектор
+public:
+	Table();//пустой конструктор
+	Table(size_t rows, size_t cols);//инициализирующий конструктор
+	const int& get_value(size_t row, size_t col);  // прочитать значение из ячейки со строкой row, столбцом col.
+	void set_value(size_t row, size_t col, const int& value);  // записать число в ячейку со строкой row, столбцом col.
+	size_t n_rows();  // вернуть число строк в таблице.
+	size_t n_cols();  // вернуть число столбцов в таблице.
+	void print();  // вывести на экран всю таблицу.
+	double average();  // вернуть среднее арифметическое всех значений таблицы.
+};
+//Задание 4
+class Complex {
+
+	double re, im;
+public:
+	Complex(double x = 0, double y = 0);
+	double Abs();
+	double Arg();
+	void Print();
+	void TrigPrint();
+	Complex Add(Complex z);
+	Complex Sub(Complex z);
+	Complex Mult(Complex z);
+};
+
+//Задание 5
+struct Box {
+	int id, w, v;
+};
+
+class Stock {
+private:
+	vector<Box> boxes;
+	int next_id = 0;
+public:
+	void Add(int w, int v);
+	int GetByW(int min_w);
+	int GetByV(int min_v);
+};
--- Source.cpp
+++ Source.cpp
@@ -1,51 +1,136 @@
 #include "Header.h"
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-Shoes::Shoes(string nameShoes, double weightShoes, int sizeShoes, string genderShoes) {
-	this->name = nameShoes;
-	this->weight = weightShoes;
-	this->size = sizeShoes;
-	this->gender = genderShoes;
+void Bell::sound() {
+
+	for (int i = 0; i < 20; i++) {
+		cout << "ding " << "dong ";
+	}	
+};
+
+void OddEvenSeparator::add_number(int n) {
+	if (n % 2 == 0) {
+		evenInt.push_back(n);
+	}
+	else {
+		oddInt.push_back(n);
+	}
+};
+
+void OddEvenSeparator::even() {
+	cout << "Четные числа:" << endl;
+	for (int num : evenInt) {
+		cout << num << "\t";
+	}
+	cout << endl;
+};
+void OddEvenSeparator::odd() {
+	cout << "Нечетные числа:" << endl;
+	for (int num : oddInt) {
+		cout << num << "\t";
+	}
+};
+void menuTable() {
+	cout << "Меню" << endl;
+	cout << "1. вызвать пустой конструктор" << endl;
+	cout << "2. вызвать инициализирующий конструктор" << endl;
+	cout << "3. прочитать значение из ячейки с заданными строкой и столбцом" << endl;
+	cout << "4. записать число в ячейку с заданными строкой и столбцом" << endl;
+	cout << "5. вернуть число строк в таблице" << endl;
+	cout << "6. вернуть число столбцов в таблице" << endl;
+	cout << "7. вывести на экран таблицу" << endl;
+	cout << "8. вернуть среднее арифметическое значений таблицы" << endl;
+	cout << "0. выйти" << endl;
+	cout << "Ваш выбор: ";
 }
 
-void Shoes::print() {
-	cout << name << "\t Г‚ГҐГ± "
-		<< weight << "\t ГђГ Г§Г¬ГҐГ° "
-		<< size << " \t ГЊГ®Г¤ГҐГ«Гј"
-		<< gender << endl;
+Table::Table() {
+	cout << "Привет! я пустой конструктор" << endl;
 }
-
-//Г±Г®Г°ГІГЁГ°Г®ГўГЄГ  ГЇГ® Г°Г Г§Г¬ГҐГ°Гі
-
-void sortBubbleSize(vector<Shoes>& shoes) {
+Table::Table(size_t rows, size_t cols):tables(rows, vector<int>(cols, 0)) {
 	
-
-	for (int i = 0; i < shoes.size(); i++) {
-		for (int j = i; j < shoes.size(); j++) {
-			if (shoes[i].size < shoes[j].size) {
-				Shoes temp = shoes[i];
-				shoes[i] = shoes[j];
-				shoes[j] = temp;
-			}
-		}
-	}
-
 }
 
-//СЃРѕСЂС‚РёСЂРѕРІРєР° РїРѕ РІРµСЃСѓ РІРѕР·СЂР°СЃС‚Р°РЅРёРµ
-void sortBubbleWeight(vector<Shoes>& shoes) {
+const int& Table::get_value(size_t row, size_t col) {//ссылка на элемент
+	if (row >= tables.size()|| col >= tables[0].size())
+		throw out_of_range("Индекс вне диапазона");
+	
+	return tables[row][col];
+}
+void Table::set_value(size_t row, size_t col, const int& value) {//изменение элемента
 
-	for (int i = 0; i < shoes.size(); i++) {
-		for (int j = i; j < shoes.size(); j++) {
-			if (shoes[i].weight > shoes[j].weight) {
-				Shoes temp = shoes[i];
-				shoes[i] = shoes[j];
-				shoes[j] = temp;
-			}
+	tables[row][col] = value;
+}
+size_t Table::n_rows() {
+	return tables.size();//число строк
+}
+size_t Table::n_cols() {
+	if (tables.size() == 0)
+		return 0;
+	return tables[0].size();//число столбцов
+}
+void Table::print() {
+	for (vector<int> row : tables) {
+		for (int elem : row) 
+			cout << elem << " ";
+		cout << '\n';
+	}
+}
+double Table::average() {
+	double s = 0;
+	for (std::vector<int> row : tables) 
+		for (int elem : row)
+			s += elem;
+	return s / (n_rows() * n_cols());
+}
+//реализация Complex
+Complex::Complex(double x, double y) : re(x), im(y) {}
+double Complex::Abs() { return sqrt(re * re + im * im); }
+double Complex::Arg() { return atan2(im, re); }
+
+void Complex::Print() {
+	cout << re << (im >= 0 ? " + " : " - ") << abs(im) << "i" << endl;
+}
+
+void Complex::TrigPrint() {
+	double r = Abs();
+	double phi = Arg();
+	cout << r << " * (cos(" << phi << ") + i*sin(" << phi << "))" << endl;
+}
+
+Complex Complex::Add(Complex z) { return Complex(re + z.re, im + z.im); }
+Complex Complex::Sub(Complex z) { return Complex(re - z.re, im - z.im); }
+Complex Complex::Mult(Complex z) {
+	return Complex(re * z.re - im * z.im, re * z.im + im * z.re);
+}
+
+//реализация Stock
+void Stock::Add(int w, int v) {
+	boxes.push_back({ next_id++, w, v });
+	cout << "Коробка добавлена! Присвоен ID: " << next_id - 1 << endl;
+}
+
+int Stock::GetByW(int min_w) {
+	for (int i = (int)boxes.size() - 1; i >= 0; --i) {
+		if (boxes[i].w >= min_w) {
+			int id = boxes[i].id;
+			boxes.erase(boxes.begin() + i);
+			return id;
 		}
 	}
+	return -1;
+}
+
+int Stock::GetByV(int min_v) {
+	for (int i = (int)boxes.size() - 1; i >= 0; --i) {
+		if (boxes[i].v >= min_v) {
+			int id = boxes[i].id;
+			boxes.erase(boxes.begin() + i);
+			return id;
+		}
+	}
+	return -1;
 }
